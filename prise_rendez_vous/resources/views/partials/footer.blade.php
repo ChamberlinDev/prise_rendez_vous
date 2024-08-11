a90cc63681ac6799fe18c6b9c73601d53fbb4cf4
ChamberlinDev <himalayaommet@gmail.com>
2024-08-11 03:16:17 +0000

    second

--- prise_rendez_vous/resources/views/partials/footer.blade.php
+++ prise_rendez_vous/resources/views/partials/footer.blade.php
@@ -1,1 +1,7 @@
-  
+<footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>GLSUPTECH &copy; Memoire 2024</span>
+                    </div>
+                </div>
+            </footer>
---
+++ prise_rendez_vous/resources/views/partials/slidebar.blade.php
@@ -0,0 +1,49 @@
+ <!-- Sidebar -->
+ <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+<!-- Sidebar - Brand -->
+<a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+    <div class="sidebar-brand-icon rotate-n-15">
+        <i class="fas fa-laugh-wink"></i>
+    </div>
+    <div class="sidebar-brand-text mx-3">CLINIQUE KEUR MARIE</div>
+</a>
+
+<!-- Divider -->
+<hr class="sidebar-divider">
+
+<!-- Nav Item - Dashboard -->
+<li class="nav-item active">
+    <a class="nav-link" href="index.html">
+        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <span>Rendez-vous</span></a>
+</li>
+
+<!-- Divider -->
+<!-- <hr class="sidebar-divider"> -->
+
+<li class="nav-item active">
+    <a class="nav-link" href="index.html">
+        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <span>Services</span></a>
+</li>
+<li class="nav-item active">
+    <a class="nav-link" href="index.html">
+        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <span>Historique</span></a>
+</li>
+
+<!-- Nav Item - Pages Collapse Menu -->
+
+
+<!-- Nav Item - Utilities Collapse Menu -->
+
+
+<!-- Divider -->
+<hr class="sidebar-divider">
+
+<!-- Sidebar Toggler (Sidebar) -->
+
+
+</ul>
+<!-- End of Sidebar -->
